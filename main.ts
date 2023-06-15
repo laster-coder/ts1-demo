de34d491dc2405d9f2ca29b0f2c164ab6559620f
unknown <ggx9912@163.com>
2023-06-15 22:46:02 +0800

    我的第一个ts应用

---
+++ main.ts
@@ -0,0 +1,25 @@
+var div = document.createElement("div");
+div.style.border = "1px solid red";
+div.style.width = "100px";
+div.style.height = "100px";
+div.id = "demo";
+document.body.appendChild(div);
+var f = false;
+var position = [0, 0];
+div.onmousedown = (e) => {
+	f = true;
+};
+document.onmousemove = (e) => {
+	if (f == true) {
+		var deltaX = e.clientX - position[0];
+		var deltaY = e.clientY - position[1];
+		var top = parseInt(div.style.top!) || 0;
+		var left = parseInt(div.style.left!) || 0;
+		div.style.top = top + deltaY + "px";
+		div.style.left = left + deltaX + "px";
+		position = [e.clientX, e.clientY];
+	}
+};
+div.onmouseup = (e) => {
+	f = false;
+};
